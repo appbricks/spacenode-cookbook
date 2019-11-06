638902e8e01a1f3cb2866962038717d08f835d17
Mevan Samaratunga <mevansam@gmail.com>
2019-11-06 16:21:19 +0400

    cleaned up logic

--- src/build/build-release.sh
+++ src/build/build-release.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 
-set -ex
-
 function usage {
-    echo -e "\nUSAGE: ./build-release.sh [release DOCKER_REPO DOCKER_USER DOCKER_PASSWORD]"
-    echo -e "\n    release          Specifying \"release\" instructs the script to build the Docker image and push it to a remote repository"
-    echo -e "    DOCKER_REPO      The name of the repository in Docker hub or address of private repository server"
-    echo -e "    DOCKER_USER      User name to login as to the remote repository"
-    echo -e "    DOCKER_PASSWORD  The users password\n"
-    exit 1
+  echo -e "\nUSAGE: ./build-release.sh [publish DOCKER_REPO DOCKER_USER DOCKER_PASSWORD]"
+  echo -e "\n    publish          Specifying \"publish\" instructs the script to build the Docker image and push it to a remote repository"
+  echo -e "    DOCKER_REPO      The name of the repository in Docker hub or address of private repository server"
+  echo -e "    DOCKER_USER      User name to login as to the remote repository"
+  echo -e "    DOCKER_PASSWORD  The users password\n"
+  exit 1
 }
 
-case $1 in
-help)
-    usage
-    ;;
-*)
-    [[ $# -eq 0 ]] || usage
-    ;;
-esac
-
-TAG="$(git tag -l --points-at HEAD)"
-if [[ "$1" == "release" ]] && [[ -z "$TAG" ]] ; then
-    echo "To build and push the release image their must be a version tag at the head of the branch."
-    exit 1
+if [[ $1 == help ]]; then
+  usage
+  exit 0
 fi
 
 docker ps -a | awk '/^[0-9a-f]/{ print $1 }' | xargs docker rm -f
-docker rmi vpn-server
+docker rmi vpn-server 2>&1 >/dev/null
+
+set -e
 docker build . -t vpn-server
 
-if [[ -n $TAG && -n $3 && -n $4 ]]; then
+if [[ "$1" == "publish" ]]; then
+  
+  if [[ -n $2 && -n $3 && -n $4 ]]; then
+
+    TAG="$(git tag -l --points-at HEAD)"
+    if [[ -z "$TAG" ]] ; then
+      echo "To build and push the release image their must be a version tag at the head of the branch."
+      exit 1
+    fi
+
     docker login -u $3 -p $4
 
     docker tag vpn-server $2/vpn-server:latest
@@ -41,4 +40,8 @@ if [[ -n $TAG && -n $3 && -n $4 ]]; then
     docker rmi $2/vpn-server:latest
     docker rmi $2/vpn-server:$TAG
     docker rmi vpn-server
+  else
+    echo "To publish DOCKER_REPO, DOCKER_USER and DOCKER_PASSWORD arguments are required."
+    exit 1
+  fi  
 fi
