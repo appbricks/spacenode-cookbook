e7094e1f5f77305293e5939806b537a154a0a4c8
Mevan Samaratunga <mevansam@gmail.com>
2019-11-06 21:12:13 +0400

    removed trace

--- src/build/run-ci-build.sh
+++ src/build/run-ci-build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 build_script_path=$(dirname $BASH_SOURCE)
-set -ex
-
-echo "'$DOCKER_REPO' '$DOCKER_USER' '$DOCKER_PASSWORD'"
+set -e
 
 if [[ "$TRAVIS_BRANCH" == "master" && -n "$TRAVIS_TAG" ]]; then
     echo "Git commit is in master branch and has a commit tag - building and publishing the docker image..."
