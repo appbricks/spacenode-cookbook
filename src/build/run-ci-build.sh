dd43d41539a0a1dce3b1a719a8d0ed03d0b90a21
Mevan Samaratunga <mevansam@gmail.com>
2019-11-06 21:09:37 +0400

    fixing travis build

--- src/build/run-ci-build.sh
+++ src/build/run-ci-build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 build_script_path=$(dirname $BASH_SOURCE)
-set -e
+set -ex
 
-env
+echo "'$DOCKER_REPO' '$DOCKER_USER' '$DOCKER_PASSWORD'"
 
-if [[ -n "$TRAVIS_BRANCH" == "master" && -n "$TRAVIS_TAG" ]]; then
+if [[ "$TRAVIS_BRANCH" == "master" && -n "$TRAVIS_TAG" ]]; then
     echo "Git commit is in master branch and has a commit tag - building and publishing the docker image..."
-    {build_script_path}/build-release.sh publish $DOCKER_REPO $DOCKER_USER $DOCKER_PASSWORD
+    ${build_script_path}/build-release.sh publish $DOCKER_REPO $DOCKER_USER $DOCKER_PASSWORD
 else
     echo "Not a release commit. Validating docker container image build only..."
-    {build_script_path}/build-release.sh
+    ${build_script_path}/build-release.sh
 fi
