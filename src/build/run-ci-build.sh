5002f202328423f21bbc66cd73733a76986ed0f5
Mevan Samaratunga <mevansam@gmail.com>
2019-11-06 21:25:08 +0400

    fixing build

--- src/build/run-ci-build.sh
+++ src/build/run-ci-build.sh
@@ -3,13 +3,12 @@
 build_script_path=$(dirname $BASH_SOURCE)
 set -e
 
-echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
-echo "TRAVIS_TAG=$TRAVIS_TAG"
-
-if [[ "$TRAVIS_BRANCH" == "master" && -n "$TRAVIS_TAG" ]]; then
+if [[ -n "$TRAVIS_TAG" ]]; then
     echo "Git commit is in master branch and has a commit tag - building and publishing the docker image..."
     ${build_script_path}/build-release.sh publish $DOCKER_REPO $DOCKER_USER $DOCKER_PASSWORD
-else
+elif [[ "$TRAVIS_BRANCH" != "master" ]]; then
     echo "Not a release commit. Validating docker container image build only..."
     ${build_script_path}/build-release.sh
+else
+    echo "Commit to master branch without tag. Skipping build..."
 fi
