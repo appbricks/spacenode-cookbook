f855e608fa2eef5043b903ce88c46b2fe38b0de0
Mevan Samaratunga <mevansam@gmail.com>
2022-12-08 19:11:14 -0500

    script to retrieve latest bastion image

---
+++ src/build/get-cloud-image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Determine MyCS bastion image for environment
+# (NOTE: this needs to be rethought)
+images=$(aws ec2 describe-images --output json \
+  --region us-east-1 --filters "Name=name,Values=appbricks-bastion-inceptor*")
+
+if [[ $1 == prod ]]; then
+  prod_images=$(echo "$images" | jq '[.Images[] | select(.Name|test("appbricks-bastion-inceptor_\\d+\\.\\d+\\.\\d+"))'])
+  appbricks_cloud_image=$(echo "$prod_images" | jq -r 'sort_by(.Name | split("_")[1] | split(".") | map(tonumber))[-1] | .Name')
+
+else
+  dev_images=$(echo "$images" | jq '[.Images[] | select(.Name|test("appbricks-bastion-inceptor_D.*"))]')
+  appbricks_cloud_image=$(echo "$dev_images" | jq -r 'sort_by(.Name | split("_D.")[1] | split(".") | map(tonumber))[-1] | .Name')
+fi
+
+echo $appbricks_cloud_image
