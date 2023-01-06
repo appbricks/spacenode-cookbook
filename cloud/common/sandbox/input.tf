2788baeb9038b633864110321e44e61f97484f39
Mevan Samaratunga <mevansam@gmail.com>
2023-01-05 23:06:45 -0500

    updated dev image version

--- cloud/common/sandbox/input.tf
+++ cloud/common/sandbox/input.tf
@@ -253,7 +253,7 @@ variable "mycs_node_id_key" {
 # Bastion image
 #
 variable "bastion_image_name" {
-  default = "appbricks-bastion-inceptor_D.221130164135"
+  default = "appbricks-bastion-inceptor_D.230105201133"
 }
 
 #
