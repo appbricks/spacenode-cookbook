396ec3c25d030888273970bd5a0da7e8de3d6baf
Mevan Samaratunga <mevansam@gmail.com>
2023-01-26 21:18:39 -0500

    fixed image name

--- src/build/get-cloud-image.sh
+++ src/build/get-cloud-image.sh
@@ -3,14 +3,14 @@
 # Determine MyCS bastion image for environment
 # (NOTE: this needs to be rethought)
 images=$(aws ec2 describe-images --output json \
-  --region us-east-1 --filters "Name=name,Values=appbricks-bastion-inceptor*")
+  --region us-east-1 --filters "Name=name,Values=appbricks-bastion*")
 
 if [[ $1 == prod ]]; then
-  prod_images=$(echo "$images" | jq '[.Images[] | select(.Name|test("appbricks-bastion-inceptor_\\d+\\.\\d+\\.\\d+"))'])
+  prod_images=$(echo "$images" | jq '[.Images[] | select(.Name|test("appbricks-bastion_\\d+\\.\\d+\\.\\d+"))'])
   appbricks_cloud_image=$(echo "$prod_images" | jq -r 'sort_by(.Name | split("_")[1] | split(".") | map(tonumber))[-1] | .Name')
 
 else
-  dev_images=$(echo "$images" | jq '[.Images[] | select(.Name|test("appbricks-bastion-inceptor_D.*"))]')
+  dev_images=$(echo "$images" | jq '[.Images[] | select(.Name|test("appbricks-bastion_D.*"))]')
   appbricks_cloud_image=$(echo "$dev_images" | jq -r 'sort_by(.Name | split("_D.")[1] | split(".") | map(tonumber))[-1] | .Name')
 fi
 
