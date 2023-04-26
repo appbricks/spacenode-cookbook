0629d29a40ff9e07e018967701d5a675d1b283ca
Mevan Samaratunga <mevansam@gmail.com>
2023-04-25 21:30:29 -0400

    fixed validation

--- cloud/common/sandbox/vagrant-input.tf
+++ cloud/common/sandbox/vagrant-input.tf
@@ -13,8 +13,8 @@
 #
 # @order: 101
 # @tags: recipe,target-undeployed,target-deployed
-# @accepted_values: [0-9]+
-# @accepted_values_message: Must be a numeric value
+# @value_inclusion_filter: ^[0-9]+$
+# @value_inclusion_filter_message: Must be a numeric value
 #
 variable "bastion_memory_size" {
   description = "How much memory in KB to allocate for the bastion instance."
