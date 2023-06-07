bd26308498ba55f504cf1db819051651a9ff497f
Mevan Samaratunga <mevansam@gmail.com>
2023-06-07 17:00:24 -0400

    fixed issue where changes to data disk was being lost between rebuilds

--- src/utils/cmd/vboxmanage-exec/main.go
+++ src/utils/cmd/vboxmanage-exec/main.go
@@ -7,10 +7,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strings"
+	"time"
 
 	"github.com/mevansam/goutils/logger"
 	"github.com/mevansam/goutils/run"
+	"github.com/mevansam/goutils/utils"
 
 	. "appbricks.io/mycs-cookbook-utils/internal"
 )
@@ -22,19 +25,26 @@ type output struct {
 	Msgs []string `json:"msgs"`
 }
 
+var options struct {
+	shutdown bool
+}
+
+var (
+	vboxmanage   run.CLI
+	outputBuffer bytes.Buffer
+)
+
 func main() {
 
 	var (
 		err error
-
-		vboxmanage   run.CLI
-		outputBuffer bytes.Buffer
 
 		jsonOutput []byte
 	)
 
 	logger.Initialize()
 
+	flag.BoolVar(&options.shutdown, "shutdown", false, "Shutdown first before executing VBoxManage cli command.")
 	flag.Parse()
 
 	output := output{
@@ -53,6 +63,11 @@ func main() {
 
 	} else {
 		args := flag.Args()
+
+		if options.shutdown {
+			shutdownVM(args[1])
+		}
+
 		err = vboxmanage.RunWithEnv(args, os.Environ())
 		_ = os.WriteFile("vboxmanage.out", outputBuffer.Bytes(), 0644)
 		if err != nil {
@@ -66,4 +81,46 @@ func main() {
 	}
 
 	output.Msgs = strings.Split(strings.TrimSuffix(outputBuffer.String(), LineBreak), LineBreak)
+}
+
+func shutdownVM(name string) {
+	defer outputBuffer.Reset()
+
+	var (
+		err error
+	)
+
+	getVMStatus := func() string {
+		defer outputBuffer.Reset()
+		if err = vboxmanage.Run([]string{ "showvminfo", name }); err == nil {
+			results := utils.ExtractMatches(outputBuffer.Bytes(), map[string]*regexp.Regexp{
+				"status": regexp.MustCompile(`^State:\s+(.*) \(.*\)$`),
+			})
+			if s := results["status"]; len(s) > 0 && len(s[0]) == 2 {
+				return s[0][1]
+			}			
+		}
+		log.Fatalf("Failed to retrieve VM Status '%s':\n%s", name, outputBuffer.String())
+		return ""
+	}
+
+	if getVMStatus() != "powered off" {
+		// send shutdown signal to vm
+		if err = vboxmanage.Run([]string{ "controlvm", name, "acpipowerbutton" }); err != nil {
+			log.Fatalf("Unable to shutdown VM '%s':\n%s", name, outputBuffer.String())
+		}
+		if !utils.InvokeWithTimeout(
+			func() {
+				for getVMStatus() != "powered off" {
+					time.Sleep(time.Second)
+				}
+			},
+			time.Second * 30,
+		) {
+			// force power off
+			if err = vboxmanage.Run([]string{ "controlvm", name, "poweroff" }); err != nil {
+				log.Fatalf("Unable to shutdown VM '%s':\n%s", name, outputBuffer.String())
+			}
+		}
+	}
 }
